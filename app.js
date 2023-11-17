8f41607acc67a5f5298d71aff5a6e858fed91c2b
Vijaya Mishra <134155725+Vijayaa21@users.noreply.github.com>
2023-11-17 13:09:22 +0530

    Update app.js
    added forecast and to get current loacation weather

--- app.js
+++ app.js
@@ -21,21 +21,28 @@ if (miN < 10) {
 daY.innerHTML = `${daYes}    ${houR}:${miN}`;
 
 function searchCity(city) {
-  let apiKey = "534360e007e0a5b20997e4664a33ded6";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  let apiKey = "6b4f3dae57b3ed708ofd511b96092atd";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
 
   axios.get(apiUrl).then(showWeather);
 }
 function showWeather(response) {
-  document.querySelector("h3").innerHTML = response.data.name;
+  document.querySelector("h3").innerHTML = response.data.city;
   document.querySelector("#temp").innerHTML = Math.round(
-    response.data.main.temp
+    response.data.temperature.current
   );
-  document.querySelector("#humid").innerHTML = response.data.main.humidity;
+  document.querySelector("#humid").innerHTML =
+    response.data.temperature.humidity;
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
-  document.querySelector("#descrip").innerHTML = response.data.weather[0].main;
+  document.querySelector(
+    "#icon"
+  ).innerHTML = `<img src="${response.data.condition.icon_url}" />`;
+  document.querySelector("#descrip").innerHTML =
+    response.data.condition.description;
+
+  getForecast(response.data.city);
 }
 
 function handleSubmit(event) {
@@ -43,8 +50,60 @@ function handleSubmit(event) {
   let city = document.querySelector("#floatingInput").value;
   searchCity(city);
 }
+function showPosition(position) {
+  let apiKey = "6b4f3dae57b3ed708ofd511b96092atd";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${position.coords.longitude}&lat=${position.coords.latitude}&key=${apiKey}`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+function getLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = "6b4f3dae57b3ed708ofd511b96092atd";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  let forecastHtml = "";
+
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
+      <div class="col">
+          <div class="day">${formatDay(day.time)}</div>
+          <div class="icon">
+            <img src="${day.condition.icon_url}" alt="">
+          </div>
+          <div class="temperature">
+            <strong> ${Math.round(
+              day.temperature.maximum
+            )}º </strong>${Math.round(day.temperature.minimum)}º
+          </div> 
+        </div>
+    `;
+    }
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
 
 let form = document.querySelector("form");
 form.addEventListener("submit", handleSubmit);
 
-searchCity("New York");
+let currentLocation = document.querySelector("#current");
+currentLocation.addEventListener("click", getLocation);
+
+searchCity("Delhi");
